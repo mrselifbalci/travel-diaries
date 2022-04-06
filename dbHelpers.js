a23195337e57136e5edf7e56ec93f9d4bed1b159
Elif Balci <mrselifbalci@gmail.com>
2022-04-06 19:53:57 +0100

    destination functions created

--- dbHelpers.js
+++ dbHelpers.js
@@ -1,31 +1,68 @@
 const knex = require('knex');
-const config = require('./knexfile');
-const db = knex(config.development);
+const config = require('./knexfile')
+const db = knex(config.development)
 
-
-async function addUser(user){
+//USERS
+async function addUser(user) {
   await db('users').insert(user)
   return db('users').where({username:user.username})
+    
 }
 
 function getAllUsers(){
-    return db('users')
-} 
+  return db('users').orderBy('id','desc')   
+}
 
-function findUserByUsername (username){
-  return db('users').where({username:username}).first();
+function findUserByUsername(username){
+    return db("users").where({username:username}).first(); 
+}
+
+function findUserById(id){ 
+    return db("users").where({id:id}).first();
 }
 
 function removeUser(id){
-  return db('users')
-  .where({id:id})
-  .del()
+ return db("users")
+ .where({id:id})
+ .del()
+}
+
+//DESTINATIONS 
+
+function getAllDestinations (){
+  return db('destinations').orderBy('id','desc') 
+}
+
+async function addDestination (newDestination,user_id){
+  await db("destinations")
+  .where({user_id:user_id})
+  .insert(newDestination)
+}
+
+function removeDestination (id){
+  return db("destinations")
+ .where({id:id})
+ .del()
+}
+
+function updateDestination (id,newDestination){
+ return db("destinations")
+ .where({id:id})
+ .update(newDestination)
+
 }
 
 
-module.exports = {
+
+module.exports={
     addUser,
     getAllUsers,
     findUserByUsername,
-    removeUser
+    findUserById,
+    removeUser,
+    getAllDestinations,
+    addDestination,
+    removeDestination,
+    updateDestination
+
 }
--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const bcrypt = require('bcryptjs')
 app.get('/',(req,res)=>{
   res.status(200).json({message:"Welcome to the server"})
 })
- app.get('/users',(req,res)=>{
+
+app.get('/users',(req,res)=>{
   Travels.getAllUsers()
   .then(users=>{
     res.status(200).json(users)
@@ -90,6 +91,71 @@ app.delete("/users/:id",(req,res)=>{
 
 
 })
+
+//DESTINATIONS ROUTES
+
+app.get('/destinations',(req,res)=>{
+  Travels.getAllDestinations()
+  .then(destinations=>{
+    res.status(200).json(destinations)
+  })
+  .catch(error=>{
+    res.status(500).json({message:"cannot get destinations"})
+  })
+})
+
+app.post("/users/:id/destinations",(req,res)=>{
+  const {id}=req.params;
+  const newDestination = req.body;
+    if(!newDestination.user_id){
+      newDestination["user_id"]= parseInt(id,10)
+    }
+Travels.findUserById(id)
+.then(user=>{
+  if(!user){
+    res.status(404).json({message:"user does not exist"})
+  }
+  if(!newDestination.title || !newDestination.description){
+    res.status(400).json({message:"All fields must be complete"})
+  }
+Travels.addDestination(newDestination,id)
+.then(destination=>{
+  res.status(200).json(destination)
+})
+.catch(error=>{
+  res.status(500).json({message:"server failed"})
+})
+
+
+})
+})
+
+app.delete("/destinations/:id",(req,res)=>{
+  const {id}=req.params;
+  Travels.removeDestination(id)
+  .then(count=>{
+    if(count>0){
+      res.status(200).json({message:"Destination is deleted"})
+    }else{
+      res.status(404).json({message:"No destination with that id"})
+    }
+  })
+  .catch(error=>{
+    res.status(500).json(error)
+  })
+
+})
+
+app.patch("/destinations/:id",(req,res)=>{
+  const {id}=req.params;
+  Travels.updateDestination(id,req.body)
+  .then(destination=>{
+    res.status(200).json({message:"destination updated"})
+  })
+  .catch(error=>{
+    res.status(500).json(error)})
+})
+
 
 
 

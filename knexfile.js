4e14f40f55de015621ece706f36ee737308bded4
Elif Balci <mrselifbalci@gmail.com>
2022-04-13 08:47:44 +0100

    a

--- knexfile.js
+++ knexfile.js
@@ -19,7 +19,10 @@ module.exports = {
     connection:process.env.DATABASE_URL,
     pool:{
       min:2,
-      max:10
+      max:10,
+      ssl: {
+        rejectUnauthorized: false,
+    }
     },
     migrations:{
       tablename:'knex_migrations',
